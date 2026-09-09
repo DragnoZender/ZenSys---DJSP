906478a87522e81f4e51f185506e3f52dbc8afef
Pratik Patil <pratikpatil09414@gmail.com>
2026-09-09 16:32:10 +0530

    job-consumer-service initialized

---
+++ Backend/job-consumer-service/src/main/java/com/zensys/job_consumer_service/JobConsumerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.zensys.job_consumer_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobConsumerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobConsumerServiceApplication.class, args);
+	}
+
+}
---
+++ Backend/job-consumer-service/src/test/java/com/zensys/job_consumer_service/JobConsumerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zensys.job_consumer_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobConsumerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
