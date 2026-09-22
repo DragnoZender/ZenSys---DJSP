d457532fa4a58aef347203ad29d5a26f290ad939
Pratik Patil <pratikpatil09414@gmail.com>
2026-09-22 16:22:02 +0530

    fixed job mapping issue in watcher-service

--- Backend/watcher-service/src/main/java/com/zensys/watcher_service/model/Job.java
+++ Backend/watcher-service/src/main/java/com/zensys/watcher_service/model/Job.java
@@ -2,6 +2,9 @@ package com.zensys.watcher_service.model;
 
 import java.time.Instant;
 
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -15,52 +18,51 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(
-        name = "jobs",
-        indexes = {
+@Table(name = "jobs", indexes = {
                 @Index(name = "idx_schedule_time", columnList = "schedule_time"),
                 @Index(name = "idx_watcher_poll", columnList = "next_run_time, status, last_polled_time")
-        }
-)
+})
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class Job {
 
-    @Id
-    @Column(length = 26, nullable = false, updatable = false)
-    private String id;
+        @Id
+        @Column(length = 26, nullable = false, updatable = false)
+        private String id;
 
-    @Column(nullable = false)
-    private String name;
+        @Column(nullable = false)
+        private String name;
 
-    @Enumerated(EnumType.STRING)
-    @Column(name = "schedule_type", nullable = false)
-    private ScheduleType scheduleType;
+        @Enumerated(EnumType.STRING)
+        @Column(name = "schedule_type", nullable = false)
+        private ScheduleType scheduleType;
 
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
-    private JobStatus status;
+        @Enumerated(EnumType.STRING)
+        @Column(nullable = false)
+        private JobStatus status;
 
-    @Column(name = "schedule_time")
-    private Instant scheduleTime;
+        @Column(name = "schedule_time")
+        private Instant scheduleTime;
 
-    @Column(name = "next_run_time")
-    private Instant nextRunTime;
+        @Column(name = "next_run_time")
+        private Instant nextRunTime;
 
-    @Column(name = "last_polled_time")
-    private Instant lastPolledTime;
+        @Column(name = "last_polled_time")
+        private Instant lastPolledTime;
 
-    @Column(name = "cron_expression")
-    private String cronExpression;
+        @Column(name = "cron_expression")
+        private String cronExpression;
 
-    @Column(columnDefinition = "jsonb")
-    private String payload;
+        @JdbcTypeCode(SqlTypes.JSON)
+        @Column(columnDefinition = "jsonb")
+        private String payload;
 
-    @Column(nullable = false)
-    private Integer retries;
+        @Column(nullable = false)
+        private Integer retries;
 
-    @Column(columnDefinition = "jsonb")
-    private String meta;
+        @JdbcTypeCode(SqlTypes.JSON)
+        @Column(columnDefinition = "jsonb")
+        private String meta;
 }
